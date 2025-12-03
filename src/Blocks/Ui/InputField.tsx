6e110228b0599f87451efb4c122e2a08fc454176
saiful10400 <saifulislam10400i5@gmail.com>
2025-12-03 19:57:38 +0600

    form data added.

--- src/Blocks/Ui/InputField.tsx
+++ src/Blocks/Ui/InputField.tsx
@@ -1,10 +1,10 @@
-import type { ChangeEventHandler } from "react";
 
 
 
-const InputField = ({ type, placeHolder, value, updaterFn }: { type: string, placeHolder: string, value: string, updaterFn:(key:string,value:string|number)=>void }) => {
+
+const InputField = ({ type, placeHolder, value, updaterFn, fieldName }: { type: string, fieldName: string, placeHolder: string, value: string, updaterFn: (key: string, value: string | number) => void }) => {
     return (
-        <input className="w-full h-full text-white text-lg pl-1 py-1 rounded-md border-2 focus:outline-0" type={type} placeholder={placeHolder} value={value} onChange={e=>updaterFn(e?.target?.valuee)} />
+        <input className="w-full h-full text-white text-lg pl-1 py-1 rounded-md border-2 focus:outline-0" type={type} placeholder={placeHolder} value={value} onChange={e => updaterFn(fieldName,e?.target?.value)} />
     );
 };
 
