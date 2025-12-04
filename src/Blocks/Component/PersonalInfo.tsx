9af3922bdb7456f9a17def48481c4a21cd10400d
saiful10400 <saifulislam10400i5@gmail.com>
2025-12-04 13:51:16 +0600

    personal info page full with input field.

--- src/Blocks/Component/PersonalInfo.tsx
+++ src/Blocks/Component/PersonalInfo.tsx
@@ -8,7 +8,10 @@ const PersonalInfo = ({updaterFn,formData,updateProgressCount}:{updateProgressCo
   
     return (
         <div className="grid grid-cols-2 gap-3">
-           <InputField updateProgressCount={updateProgressCount} errorMessage="Your full name missing!" fieldName="fullName" updaterFn={updaterFn}  type="text" value={formData["fullName"]} placeHolder="Your Name" />
+           <InputField updateProgressCount={updateProgressCount} errorMessage="Your full name is missing!" fieldName="fullName" updaterFn={updaterFn}  type="text" value={formData["fullName"]} placeHolder="Your Name" />
+           <InputField updateProgressCount={updateProgressCount} errorMessage="Your email missing!" fieldName="email" updaterFn={updaterFn}  type="email" value={formData["email"]} placeHolder="Your E-mail" />
+           <InputField updateProgressCount={updateProgressCount} errorMessage="Your phone number is missing!" fieldName="phone" updaterFn={updaterFn}  type="number" value={formData["phone"]} placeHolder="Your Phone number" />
+           <InputField updateProgressCount={updateProgressCount} errorMessage="Your date of birth is missing!" fieldName="dob" updaterFn={updaterFn}  type="date" value={formData["dob"]} placeHolder="Your Date of birth" />
         </div>
     );
 };
--- src/Blocks/Util/Function/updateProgress.ts
+++ src/Blocks/Util/Function/updateProgress.ts
@@ -1,5 +1,7 @@
+import initialFormData from "../../Storage/FormData";
+
 const updateProgressFn = (fn: (v: number) => void) => {
-  fn(100 / 12);
+  fn(100 / Object.keys(initialFormData).length);
 };
 
 export default updateProgressFn;
